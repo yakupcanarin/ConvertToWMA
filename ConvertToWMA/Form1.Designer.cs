686a5cfb6d7f59389bc96f2faa768b6b538599fd
Yakup (Jacob) Can Arın <ykpcn.arn@hotmail.com>
2018-09-10 14:57:14 +0300

    MP3ToWMA
    Convert mp3 files to WMA files. It's required to who drives old BMW cars (to make a media CD's)

--- ConvertToWMA/Form1.Designer.cs
+++ ConvertToWMA/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dönüştür";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 181);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(351, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seçilenler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(375, 423);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(391, 462);
+            this.MinimumSize = new System.Drawing.Size(391, 462);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ConvertToWMA/Form1.cs
+++ ConvertToWMA/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio;
+using NAudio.Lame;
+using NAudio.Wave;
 
 namespace ConvertToWMA
 {
@@ -15,6 +19,50 @@ namespace ConvertToWMA
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "MP3|*.mp3";
+            ofd.Multiselect = true;
+            ofd.ShowDialog();
+
+
+            foreach (var item in ofd.FileNames)
+            {
+                comboBox1.Items.Add(item);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var item in comboBox1.Items)
+                {
+                    using (Mp3FileReader reader = new Mp3FileReader(item.ToString()))
+                    {
+                        string data = Path.GetDirectoryName(item.ToString());
+                        string fileName = Path.GetFileNameWithoutExtension(item.ToString());
+                        if (!File.Exists(data+"\\"+fileName+".wma"))
+                        {
+                            WaveFileWriter.CreateWaveFile(data + "\\" + fileName + ".wma", reader);
+                        }
+                        else
+                        {
+                            File.Delete(data + "\\" + fileName + ".wma");
+                            WaveFileWriter.CreateWaveFile(data + "\\" + fileName + ".wma", reader);
+                        }
+                    }
+                }
+                MessageBox.Show(Parent, "Başarılı", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "HATA!", MessageBoxButtons.OK);
+            }
         }
     }
 }
